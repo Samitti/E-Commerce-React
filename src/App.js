08108d4d85c5b9e25f57f52c42d7a30be700e2a4
Samitti <samuelteweldebrhan@gmail.com>
2021-09-01 14:27:35 +0300

    connect commerce.js api and displayed the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import Products from './Components/Products/Products'
+import React, { useState, useEffect } from 'react'
+import { commerce } from './lib/commerce';
+// import Products from './Components/Products/Products'
+// import Navbar from './Components/Navbar/Navbar';
 
+import { Products, Navbar } from './Components';
+     
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProduct = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProduct();
+    }, []);
+
+
     return (
         <div>
-            <Products />
+            <Navbar />
+            <Products products={products} />
         </div>
     )
 }
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+
 import Product from './Product/Product';
+import useStyles from './styles'
 
-const products = [
-    { id: 1, name: 'Shoes', descrption: 'Running Shoes.', price: '$5' },
-    { id: 2, name: 'Macbook', descrption: 'Appl mackbook.', price: '$10' },
-]
+// const products = [
+//     { id: 1, name: 'Shoes', descrption: 'Running Shoes.', price: '$5', imgUrl: 'https://elitedigital.co.ug/wp-content/uploads/2021/08/MacBook-Air-M1-300x300.jpg' },
+//     { id: 2, name: 'Macbook', descrption: 'Appl mackbook.', price: '$10', imgUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZXx7y4zbloNwvMN7B9lXmP139ixAwM_w0NQ&usqp=CAU' },
+// ]
 
- const Products = () => {
-
+ const Products = ({ products }) => {
+    const classes = useStyles();
     return(
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
             <Grid container justify='center' spacing={4} >
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
