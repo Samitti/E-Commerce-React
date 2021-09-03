6923f9d9b713780314f1ff952326b22783c3ab30
Samitti <samuelteweldebrhan@gmail.com>
2021-09-03 16:34:59 +0300

    add full control to the cart feature

--- src/Components/Cart/CartItem/CartItem.jsx
+++ src/Components/Cart/CartItem/CartItem.jsx
@@ -3,11 +3,11 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@
 
 import useStyles from './styles';
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, handleUpdateCartQty, handleRemoveFromCart }) => {
     const classes = useStyles(); 
 
     return (
-        <Card>
+        <Card className="cart-item">
             <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
             <CardContent className={classes.cardContent}>
                 <Typography variant="h5">{item.name}</Typography>
@@ -15,11 +15,11 @@ const CartItem = ({ item }) => {
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <div className={classes.buttons}>
-                    <Button type="button" size="small">-</Button>
+                    <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type="button" size="small">+</Button> 
+                    <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</Button> 
                 </div> 
-                <Button variant="contained" type="button" color="secondary">Remove</Button>                
+                <Button variant="contained" type="button" color="secondary" onClick={() => handleRemoveFromCart(item.id)}>Remove</Button>                
             </CardActions>
         </Card>
     )
